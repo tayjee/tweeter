a33f7ea076eba84d41ba0ff262759b1206eef747
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-02-03 00:29:17 +0000

    Functional render

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,14 +5,38 @@
  */
 $(document).ready(() => {
 
-function createTweetElement (tweetObject) {
-  const time = timeago.format(tweetObject.created_at);
 
-  let tweetElement = `
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+const createTweetElement  = (tweetObject) => {
+  let $tweet = `
   <article class = "user-tweet">
   <div class = "tweet-header">
     <div class = "user-name">
-    <i class="fas fa-smile-beam"></i> 
+    <img src=${tweetObject["user"].avatars}>
     ${tweetObject["user"].name}
   </div>
     <div class = "user-handle">
@@ -25,7 +49,7 @@ function createTweetElement (tweetObject) {
     </p>
   </div>
   <div class = "footer">
-    ${time}
+    ${timeago.format(tweetObject["created_at"])}
     <div class = "icons">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -33,31 +57,20 @@ function createTweetElement (tweetObject) {
     </div>
   </div>
 </article>
-  `;
-
-  return tweetElement;
+`;
+  return $tweet;
 }
 
-const data = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const renderTweets = function(tweets) {
+const renderTweets = (tweets) =>  {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  tweets.forEach((tweetObject) => {
-    $('#tweets-container').append(createTweetElement(tweetObject));
-  });
+  for (let tweet of tweets) {
+    let $tweetPost = createTweetElement(tweet);
+    $('#tweet-container').append($tweetPost);
+  }
 };
-});
 
 renderTweets(data);
+});
+
