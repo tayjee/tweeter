2f67efe77406abf02abd1ee6613f416e6f6d57f7
Jimmy Tang <jimmy.tang.360@gmail.com>
2022-02-04 00:58:16 +0000

    Add form validation, user cannot tweet if blank or >140 char

--- public/scripts/tweet-submit.js
+++ public/scripts/tweet-submit.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
   $(".tweet-form").on("submit", function(event) {
     event.preventDefault();
     let form = $(this).serialize();
-    $.ajax('/tweets', { method: 'POST', data: form});
+    let formLength = $(this).serializeArray()[0].value.length;
+    let emptyCheck= $(this).serializeArray()[0].value.replace(/\s+/g, '').length;
+    if (formLength > 140) {
+      alert("Tweet exceeds the character limit!");
+      return;
+    } else if (emptyCheck === 0) {
+        alert("Tweet cannot be blank!");
+        return;
+      } else {
+        $.ajax('/tweets', { method: 'POST', data: form});
+      }
   });
 });
